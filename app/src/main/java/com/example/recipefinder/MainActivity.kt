ea869779da7d3f0ced5890d40d14d799d06b9bd6
4ASIM <masim4872@gmail.com>
2024-10-21 15:11:29 +0500

    stating application

---
+++ app/src/main/java/com/example/recipefinder/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.recipefinder
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.recipefinder.databinding.ActivityMainBinding
+
+
+class MainActivity : AppCompatActivity() {
+    private  var binding: ActivityMainBinding?= null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        //added
+        Handler().postDelayed({
+
+            val intent = Intent(this,MainActivity2::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+}
